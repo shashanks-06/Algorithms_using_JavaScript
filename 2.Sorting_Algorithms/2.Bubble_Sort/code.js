47aba0a983b3622dfedbc5e5388a6f7c2f3c66e7
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-05-14 17:59:14 +0530

    Merging & Bubble Sorting two arrays

--- 2.Sorting_Algorithms/2.Bubble_Sort/code.js
+++ 2.Sorting_Algorithms/2.Bubble_Sort/code.js
@@ -1,3 +1,6 @@
+// 1
+
+/*
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
@@ -12,3 +15,33 @@ function bubbleSort(arr) {
 }
 
 console.log(bubbleSort([4, 2, 3, 1, 5, 6, 8, 7]));
+*/
+
+// 2
+
+function mergeTwo(arr1, arr2) {
+  let merged = [];
+  let index1 = 0;
+  let index2 = 0;
+  let current = 0;
+
+  while (current < arr1.length + arr2.length) {
+    let arr1Done = index1 >= arr1.length;
+    let arr2Done = index2 >= arr2.length;
+
+    if (!arr1Done && (arr2Done || arr1[index1] < arr2[index2])) {
+      merged[current] = arr1[index1];
+      index1++;
+    } else {
+      merged[current] = arr2[index2];
+      index2++;
+    }
+    current++;
+  }
+  return merged;
+}
+
+var arr1 = [3, 5, 6, 10, 11, 20];
+var arr2 = [1, 2, 7, 8, 15, 19];
+
+console.log(mergeTwo(arr1, arr2));
