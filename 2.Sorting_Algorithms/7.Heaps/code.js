fd357df4141af24f767be755126d8f5a9a9d75a8
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-05-21 16:18:31 +0530

    Node Insertion

---
+++ 2.Sorting_Algorithms/7.Heaps/code.js
@@ -0,0 +1,19 @@
+class MaxHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  parentIndex(index) {
+    return Math.floor((index - 1) / 2);
+  }
+
+  leftChildIndex(index) {
+    return 2 * index + 1;
+  }
+
+  rightChildIndex(index) {
+    return 2 * index + 2;
+  }
+
+  
+}
